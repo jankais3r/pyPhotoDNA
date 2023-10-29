9d230a52de2cc5cb6a29243820b52b24e768711f
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2023-10-29 21:26:16 +0100

    New download link for Windows/Linux

--- install.bat
+++ install.bat
@@ -5,37 +5,14 @@ echo Welcome to the pyPhotoDNA installer.
 echo The script will now setup a pyPhotoDNA environment for you. Please be patient.
 
 echo.
-echo Downloading AXIOM (4GB, might take a while)...
-bitsadmin /transfer "Downloading AXIOM" /priority HIGH https://magnetforensics.com/dl/axiom %cd%\axiom.zip > nul
+echo Downloading FTK (3.3GB, might take a while)...
+bitsadmin /transfer "Downloading FTK" /priority HIGH https://d1kpmuwb7gvu1i.cloudfront.net/AD_FTK_7.0.0.iso %cd%\AD_FTK_7.0.0.iso > nul
 
 echo.
-echo Extracting AXIOM Installer...
-powershell Expand-Archive axiom.zip -DestinationPath .\axiom
-
-del axiom.zip
-cd axiom
-
-echo.
-echo Downloading InnoExtract...
-bitsadmin /transfer "Downloading InnoExtract" /priority HIGH  https://constexpr.org/innoextract/files/innoextract-1.9-windows.zip %cd%\innoextract.zip > nul
-
-echo.
-echo Extracting InnoExtract...
-powershell Expand-Archive innoextract.zip -DestinationPath .
-
-echo.
-echo Extracting PhotoDNAx64.dll (might take a while)...
-for %%A in (*.exe) do (
-    set "axiom=%%~nA.exe"
-    GOTO endloop
-)
-
-:endloop
-innoextract.exe -I "app\AXIOM Process\x64\PhotoDNAx64.dll" -s -e %axiom%
-
-move "%cd%\app\AXIOM Process\x64\PhotoDNAx64.dll" %cd%\..
-cd ..
-rmdir /s /q "%cd%\axiom"
+echo Extracting PhotoDNAx64.dll.
+powershell.exe -Command "&{$mountResult = Mount-DiskImage %cd%\\AD_FTK_7.0.0.iso; $driveLetter = ($mountResult | Get-Volume).DriveLetter; $setupPath = \"$($driveLetter):\FTK\FTK\x64\{C32BC7B7-3086-44FD-8A90-9EC47966B4B7}\Data1.cab\"; echo "'Setup file is' $setupPath"; $finalString = expand $setupPath /f:photodnax64.1.72.dll %cd%; Get-Volume $driveLetter | Get-DiskImage | Dismount-DiskImage }" > nul
+del AD_FTK_7.0.0.iso
+rename photodnax64.1.72.dll PhotoDNAx64.dll
 
 echo.
 echo.
--- install.sh
+++ install.sh
@@ -6,22 +6,7 @@ echo "The script will now setup a pyPhotoDNA environment for you. Please be pati
 echo
 
 if [ "$(uname)" == "Darwin" ]; then
-	echo "Downloading Inspector (4.4 GB, might take a while)..."
-	curl -LO https://archive.org/download/cellebrite-inspector-10.3-mac/Cellebrite_Inspector_10.3_Mac.pkg
-	
-	echo
-	echo "Extracting PhotoDNAx64.so."
-	pkgutil --expand-full Cellebrite_Inspector_10.3_Mac.pkg Inspector
-	rm Cellebrite_Inspector_10.3_Mac.pkg
-	mv "Inspector/Inspector.pkg/Payload/Applications/Inspector/Inspector 10.3/Inspector.app/Contents/Helpers/Mac/PhotoDNAx64-osx.so.1.72" PhotoDNAx64.so
-	rm -rf Inspector
-	
-	echo
-	echo
-	echo "Installation complete!"
-	echo "_____________________________"
-	echo
-	echo "To generate a PhotoDNA hash, run: python3 generateHashes.py"
+	echo "Download link for native macOS version currently unavailable."
 else
 	if ! [ -x "$(command -v curl)" ]; then
 		echo "Dependency missing. Please install 'curl' and re-run the installer."
@@ -31,36 +16,26 @@ else
 		echo "Dependency missing. Please install 'wine64' and re-run the installer."
 		exit 1
 	fi
-	if ! [ -x "$(command -v unzip)" ]; then
-		echo "Dependency missing. Please install 'unzip' and re-run the installer."
+	if ! [ -x "$(command -v cabextract)" ]; then
+		echo "Dependency missing. Please install 'cabextract' and re-run the installer."
+		exit 1
+	fi
+	if ! [ -x "$(command -v isoinfo)" ]; then
+		echo "Dependency missing. Please install 'genisoimage' and re-run the installer."
 		exit 1
 	fi
 	
-	echo "Downloading AXIOM (4GB, might take a while)..."
-	curl -Lo axiom.zip https://magnetforensics.com/dl/axiom
-	
-	echo
-	echo "Extracting AXIOM Installer..."
-	unzip axiom.zip
-	rm axiom.zip
-	
-	echo
-	echo "Downloading InnoExtract..."
-	curl -Lo innoextract.tar.xz https://constexpr.org/innoextract/files/innoextract-1.9-linux.tar.xz
-	
-	echo
-	echo "Extracting InnoExtract..."
-	tar -xf innoextract.tar.xz
-	rm innoextract.tar.xz
+	echo "Downloading FTK (3.3GB, might take a while)..."
+	curl -LO https://d1kpmuwb7gvu1i.cloudfront.net/AD_FTK_7.0.0.iso
 	
 	echo
 	echo "Extracting PhotoDNAx64.dll."
-	find . -maxdepth 1 -name "AXIOM*setup.exe" | xargs innoextract-1.9-linux/bin/amd64/innoextract -I "app/AXIOM Process/x64/PhotoDNAx64.dll" -s -e
-	mv "app/AXIOM Process/x64/PhotoDNAx64.dll" PhotoDNAx64.dll
-	rm -rf app
-	rm -rf innoextract-1.9-linux
-	rm AXIOM*setup.exe
-	rm AXIOM*setup*.bin
+	isoinfo -i AD_FTK_7.0.0.iso -x /FTK/FTK/X64/_8A89F09/DATA1.CAB > Data1.cab
+	rm AD_FTK_7.0.0.iso
+	cabextract -d tmp -q Data1.cab
+	rm Data1.cab
+	mv tmp/photodnax64.1.72.dll PhotoDNAx64.dll
+	rm -rf tmp
 	
 	echo
 	echo "Downloading minimal Python for Wine..."
